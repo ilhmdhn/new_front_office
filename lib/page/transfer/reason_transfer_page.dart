386e96808c8ec204303d5e71de9c9b75da7dc8e4
Ilham Dohaan <doha39anilham@gmail.com>
2024-06-06 17:35:03 +0700

    progress transfer

--- lib/page/transfer/reason_transfer_page.dart
+++ lib/page/transfer/reason_transfer_page.dart
@@ -1,7 +1,8 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:custom_radio_grouped_button/custom_radio_grouped_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:front_office_2/data/model/detail_room_checkin_response.dart';
+import 'package:front_office_2/data/model/room_type_model.dart';
 import 'package:front_office_2/page/style/custom_color.dart';
 import 'package:front_office_2/page/style/custom_text.dart';
 import 'package:front_office_2/tools/list.dart';
@@ -15,6 +16,15 @@ class TransferReasonPage extends StatefulWidget {
 }
 
 class _TransferReasonPageState extends State<TransferReasonPage> {
+
+  bool isLoading = true;
+  ListRoomTypeReadyResponse listRoom = ListRoomTypeReadyResponse();
+  DetailCheckinResponse? detailRoom;
+
+  void getData()async{
+    // if(detailRoom.data.roomType.)
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -33,48 +43,52 @@ class _TransferReasonPageState extends State<TransferReasonPage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               AutoSizeText("Alasan Transfer", style: CustomTextStyle.blackStandard(),),
-              Row(
-                children: [
-                  Flexible(
-                    child: CustomRadioButton(
-                      defaultSelected: "Overpax",
-                      selectedBorderColor: Colors.transparent,
-                      unSelectedBorderColor: CustomColorStyle.appBarBackground(),
-                      enableShape: true,
-                      shapeRadius: 0,
-                      spacing: 0,
-                      absoluteZeroSpacing: true,
-                      margin: const EdgeInsets.only(top: 6),
-                      horizontal: true,
-                      // absoluteZeroSpacing: true,
-                      // wrapAlignment: WrapAlignment.center,
-                      elevation: 0, // Menghilangkan bayangan
-                      buttonLables: transferReason, 
-                      buttonValues: transferReason,
-                      buttonTextStyle: ButtonTextStyle(
-                        selectedColor: Colors.white,
-                        unSelectedColor: Colors.black,
-                        textStyle: CustomTextStyle.blackMedium()
-                      ),
-                      autoWidth: true,
-                      enableButtonWrap: true,  
-                      padding: 0,                      
-                      radioButtonValue: (value){
-                      setState(() {
-                    
-                      });
-                      }, 
-                      unSelectedColor: Colors.white, 
-                      selectedColor: CustomColorStyle.appBarBackground()
-                    ),
-                  ),
-                  const Flexible(child: SizedBox())
-                ],
+              CustomRadioButton(
+                defaultSelected: "Overpax",
+                selectedBorderColor: Colors.transparent,
+                unSelectedBorderColor: CustomColorStyle.appBarBackground(),
+                enableShape: true,
+                shapeRadius: 0,
+                spacing: 0,
+                // absoluteZeroSpacing: true,
+                margin: const EdgeInsets.only(top: 6, right: 3),
+                horizontal: false,
+                // absoluteZeroSpacing: true,
+                // wrapAlignment: WrapAlignment.center,
+                elevation: 0, // Menghilangkan bayangan
+                buttonLables: transferReason, 
+                buttonValues: transferReason,
+                buttonTextStyle: ButtonTextStyle(
+                  selectedColor: Colors.white,
+                  unSelectedColor: Colors.black,
+                  textStyle: CustomTextStyle.blackMedium()
+                ),
+                autoWidth: true,
+                enableButtonWrap: false,  
+                padding: 0,                      
+                radioButtonValue: (value){
+                setState(() {
+              
+                });
+                }, 
+                unSelectedColor: Colors.white, 
+                selectedColor: CustomColorStyle.appBarBackground()
               ),
               Center(
                 child: Text("Room Type", style: CustomTextStyle.blackStandard(),),
               ),
-              
+
+              isLoading == true?
+                Flexible(
+                  child: Center(
+                    child: CircularProgressIndicator(color: CustomColorStyle.appBarBackground(),),
+                  ),
+                ):
+
+                SizedBox()
+  
+              // ListView.builder(
+              //   itemBuilder: )
             ],
           ),
         ),
